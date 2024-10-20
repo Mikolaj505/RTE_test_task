0646536b1fbb524bc973443a0d7998d3bf81570a
Mikolaj505 <mikmakub@gmail.com>
2024-10-20 13:46:36 +0200

    feat: Added basic input reading

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,35 @@
+﻿using Fusion;
+using UnityEngine;
+
+namespace MKubiak.RTETestTask.Input
+{
+    public class PlayerInputController : MonoBehaviour
+    {
+        [SerializeField] private PlayerInputActions _inputActions;
+
+        public PlayerInput PlayerInput { get; private set; }
+
+        private void Awake()
+        {
+            _inputActions = new PlayerInputActions();
+            _inputActions.PlayerMap.Enable();
+        }
+
+        private void Update()
+        {
+            PlayerInput = new()
+            {
+                Movement = _inputActions.PlayerMap.Movement.ReadValue<Vector2>(),
+                Look = _inputActions.PlayerMap.Look.ReadValue<Vector2>()
+            };
+
+            Debug.Log($"Movement {PlayerInput.Movement}, Look {PlayerInput.Look}");
+        }
+    }
+
+    public struct PlayerInput : INetworkInput
+    {
+        public Vector2 Movement;
+        public Vector2 Look;
+    }
+}
