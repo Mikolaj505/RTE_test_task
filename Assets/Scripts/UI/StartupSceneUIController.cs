4c55b3da4dbefc996695d733ed3ceb31f888db82
Mikolaj505 <mikmakub@gmail.com>
2024-10-28 23:32:50 +0100

    fix: Disabling buttons after selected host/join

--- Assets/Scripts/UI/StartupSceneUIController.cs
+++ Assets/Scripts/UI/StartupSceneUIController.cs
@@ -2,11 +2,14 @@ using Fusion;
 using JetBrains.Annotations;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MKubiak.RTETestTask.GameStartup
 {
     public class StartupSceneUIController : MonoBehaviour
     {
+        [SerializeField] private Button[] _buttonsToDisableOnGameStart;
+
         [SerializeField] private List<int> _scenesToLoadAtGameplayStart = new();
 
         private GameStarter _gameStarter = new();
@@ -15,12 +18,22 @@ namespace MKubiak.RTETestTask.GameStartup
         public void OnHostStartButton()
         {
             _gameStarter.StartGame(new GameStartInfo(_scenesToLoadAtGameplayStart.ToArray(), GameMode.Host));
+            SetButtonsInteractable(false);
         }
 
         [UsedImplicitly]
         public void OnClientStartButton()
         {
             _gameStarter.StartGame(new GameStartInfo(_scenesToLoadAtGameplayStart.ToArray(), GameMode.Client));
+            SetButtonsInteractable(false);
+        }
+
+        private void SetButtonsInteractable(bool active)
+        {
+            foreach (var button in _buttonsToDisableOnGameStart)
+            {
+                button.interactable = active;
+            }
         }
     }
 }
